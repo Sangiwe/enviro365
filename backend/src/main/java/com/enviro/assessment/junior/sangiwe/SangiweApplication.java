94c2f35043cc54b8237ad4b91552f09115729cb0
Sangiwe <nokubongasangiwe@gmail.com>
2026-09-10 14:33:01 +0200

    Scaffold Spring Boot backend project with Web, JPA, H2, Validation, Lombok

---
+++ backend/src/main/java/com/enviro/assessment/junior/sangiwe/SangiweApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.sangiwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangiweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangiweApplication.class, args);
+	}
+
+}
