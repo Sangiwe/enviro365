7956498b5ef29599bd882fc1d116cf47383b3c6f
Sangiwe <nokubongasangiwe@gmail.com>
2026-09-10 15:50:21 +0200

    Add Spring Data JPA repositories for Investor, Product, WithdrawalNotice

---
+++ backend/src/main/java/com/enviro/assessment/junior/sangiwe/repository/WithdrawalNoticeRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.junior.sangiwe.repository;
+
+import com.enviro.assessment.junior.sangiwe.entity.WithdrawalNotice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WithdrawalNoticeRepository extends JpaRepository<WithdrawalNotice, Long> {
+}
